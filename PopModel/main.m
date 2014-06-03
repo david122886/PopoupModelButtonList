213864759ac55023734da5d7eec577c7b7ba5ca8
david <liuchong1228@126.com>
2014-06-03 09:33:15 +0800

    Initial Commit

---
+++ PopModel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PopModel
+//
+//  Created by xxsy-ima001 on 14-6-3.
+//  Copyright (c) 2014年 ___xiaoxiangwenxue___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
